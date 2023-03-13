0df135e95fe5d6d48157d9f62021ab83b5040618
Clissman J. Camacho <ccamacho@saltala.com>
2023-03-13 15:29:09 -0300

    feat: :art: update

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Boaz</span>
+                    <img src="https://boaz.cl/wp-content/uploads/2021/02/logoblanco.png" class="mr-3 h-6 sm:h-9"
+                        alt="Flowbite Logo" />
                 </a>
                 <div class="flex items-center lg:order-2">
                     <RouterLink to="/login"
